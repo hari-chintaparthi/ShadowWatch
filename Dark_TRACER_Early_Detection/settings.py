bc0bb79c6f9cdc6fd447b07e86a08eb4fbc41ec7
Hari Chintaparthi <harichintaparthi19@gmail.com>
2025-07-28 10:26:32 +0530

    Initial commit

--- Dark_TRACER_Early_Detection/settings.py
+++ Dark_TRACER_Early_Detection/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'm+1edl5m-5@u9u!b8-=4-4mq&o1%agco2xpl8c!7sn7!eowjk#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
